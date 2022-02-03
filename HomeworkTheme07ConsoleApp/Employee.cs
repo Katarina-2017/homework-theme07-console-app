3db759a262201054425c239eb7eedd4eaba52ce9
katzhukova <katzhukova@gmail.com>
2022-02-03 14:42:57 +0300

    Создайте структуру «Сотрудник» со следующими полями: ID, Дата и время добавления записи, Ф.И.О., Возраст, Рост, Дата рождения, Место рождения. И создала свойства для доступа к этим полям.

---
+++ HomeworkTheme07ConsoleApp/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkTheme07ConsoleApp
+{
+    struct Employee
+    {
+        #region Поля
+
+        private int id;
+
+        private DateTime recordCreationDate;
+
+        private DateTime recordCreationTime;
+
+        private string lastName;
+
+        private string firstName;
+
+        private string patronymic;
+
+        private byte age;
+
+        private int height;
+
+        private DateTime dateOfBirth;
+
+        private string birthPlace;
+
+        #endregion
+
+        #region Свойства
+
+        public int Id { get { return this.id; } set { this.id = value; } }
+
+        public DateTime RecordCreationDate { get { return this.recordCreationDate; } set { this.recordCreationDate = value; } }
+
+        public DateTime RecordCreationTime { get { return this.recordCreationTime; } set { this.recordCreationTime = value; } }
+
+        public string LastName { get { return this.lastName; } set { this.lastName = value; } }
+
+        public string FirstName { get { return this.firstName; } set { this.firstName = value; } }
+
+        public string Patronymic { get { return this.patronymic; } set { this.patronymic = value; } }
+
+        public byte Age { get { return this.age; } set { this.age = value; } }
+
+        public int Height { get { return this.height; } set { this.height = value; } }
+
+        public DateTime DateOfBirth { get { return this.dateOfBirth; } set { this.dateOfBirth = value; } }
+
+        public string BirthPlace { get { return this.birthPlace; } set { this.birthPlace = value; } }
+
+        #endregion
+    }
+}
